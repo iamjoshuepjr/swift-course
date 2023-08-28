e6cc7b608cb4f53ecda577f414166763860943b4
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-08-28 14:01:01 -0500

    Swift Variables

--- fundamentals/variables-constants/variables.swift
+++ fundamentals/variables-constants/variables.swift
@@ -1,6 +1,17 @@
 var nickname = "Joshuép Jr.";
 var quote = "\"Luck is what happens when preparation meets opportunity.\""
 
+print("""
+     +--------------------------------------------+
+     - First way to display the variable content  -
+     +--------------------------------------------+
+     """)
 
 print(nickname)
 print(quote)
+print("""
+     +---------------------------------------------+
+     - Second way to display the variable content  -
+     +----------------------------------------------+
+     """)
+print("Nickname: \(nickname)\nQuote: \(quote)")
