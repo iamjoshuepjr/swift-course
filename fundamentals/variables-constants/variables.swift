da3646fc9651bdd5d4e2499dc3a85441b599b0d5
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-08-28 15:01:51 -0500

    Swift Variables

--- fundamentals/variables-constants/variables.swift
+++ fundamentals/variables-constants/variables.swift
@@ -1,3 +1,5 @@
+// To declare variables you need use the var keyword
+
 var nickname = "Joshuép Jr.";
 var quote = "\"Luck is what happens when preparation meets opportunity.\""
 
@@ -15,3 +17,6 @@ print("""
      +----------------------------------------------+
      """)
 print("Nickname: \(nickname)\nQuote: \(quote)")
+
+// You can declare multiple variables on a single line, separated by commas.
+var book = "iOS Programming: The Big Nerd Ranch Guide", author = "Aaron Hillegass", editorial = "Big Nerd Ranch"   
