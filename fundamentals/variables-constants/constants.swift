ecb38f31468f69ec51996cfb44d7fbe5aeb9fe6e
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-08-29 07:34:34 -0500

    Swift Constants

---
+++ fundamentals/variables-constants/constants.swift
@@ -0,0 +1,21 @@
+/* To declare constants you need use the let keyword. 
+   Swift constants are much more powerful than C constants. */
+
+let country = "United States of America"
+let capital = "Washington, D.C."
+let founding = 1776
+let language = "English"
+let current = "$"
+let largestCity = "New York City"
+
+print(
+    """
+    \n
+    Country: \(country)
+    Capital City: \(capital)
+    Founding Year: \(founding)
+    Language: \(language)
+    Current: (\(current))
+    Largest City: \(largestCity)
+    \n
+    """)
