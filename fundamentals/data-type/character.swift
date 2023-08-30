a2a6e21823cb6bcc2c3a0b76493d643717e32d29
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-08-30 08:47:39 -0500

    Swift Data Types[Character]

---
+++ fundamentals/data-type/character.swift
@@ -0,0 +1,5 @@
+/* The character data type is used to represent a single-character string.
+   We use the character keyword to create character-type variables. */
+
+var letter: Character = "J"
+print("The value of Character is: \"\(letter)\"")
