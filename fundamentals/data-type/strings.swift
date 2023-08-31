afa45b52107ae67dd6b3aa2f2bbfffa6fa59d14c
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-08-31 08:24:06 -0500

    Swift Data Types[Strings]

---
+++ fundamentals/data-type/strings.swift
@@ -0,0 +1,7 @@
+/* The string data type is used to represent textual data. 
+   We use the String keyword to create string-type variables. 
+   For example: */
+
+var language: String = "Swift"
+print("The language used to developer iOS apps is: \(language)")
+
