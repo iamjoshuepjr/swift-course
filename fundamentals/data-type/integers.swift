f0508300434ea9aaf99f65c70c61b68eeecb2a0d
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-09-01 07:35:09 -0500

    Swift Data Types[Integers]

---
+++ fundamentals/data-type/integers.swift
@@ -0,0 +1,10 @@
+/* An integer data type is used to represent a whole number with no fractional component. 
+   We use the Int keyword to create integer type variables. */
+
+
+// It can store both positive and negative whole numbers
+let birthyear: Int = 1995
+let stockQuantity: Int = 1004;
+
+// Unsigned Integers; it can olny store non-negative integers
+let age: UInt = 24;
